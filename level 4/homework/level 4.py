d2b05de0326af7a96b7de3d1a801dd46787ca61e
dimitriotkhvani <demetreotkhvani@gmail.com>
2024-06-07 21:54:03 +0400

    commit

---
+++ level 4/homework/level 4.py
@@ -0,0 +1,11 @@
+name = "dimitri otkhvani"
+age = "11 years old"
+hobbies = "my hobbies are reading books readings poems and plaing footbal"
+
+print(name)
+
+print(age)
+
+print(hobbies)
+
+print(name + ' ' + age + ' ' + hobbies)
